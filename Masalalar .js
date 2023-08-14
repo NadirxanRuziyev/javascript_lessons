c3884ea93b45facc8a523d4b4071fc79ad8cc1f9
Ruziyev_Nadirxan <nodirruziyev381@gmail.com>
2023-08-14 13:17:08 +0500

    1-amaliyot

--- Masalalar .js
+++ Masalalar .js
@@ -9,15 +9,15 @@
 // // 1-misol
 // let myName = prompt("ismingizni kiriting:"),
 //     age = +prompt("Yoshingizni kiriting:");
-// console.log(`Assalomu aleykum ${myName}
+// console.log(`Assalomu aleykum ${myName}👨‍🎓
 // Sizning yilingiz ${2023- age}
 // Yashagan oyingiz: ${age*12}
-// Yashagan Hafta: ${age*12*4+3}
+// Yashagan Hafta: ${(age*12)*4+3}
 // Yashagan kun: ${age*365}
 // Yashagan soat: ${age*365*24}
 // Yashagan daqiqa: ${age*365*24*60}
 // Yashagan sekunt: ${age*365*24*60*60}
 // `);
 
-
+// 2-misol
 
