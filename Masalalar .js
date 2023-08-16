c9449ffb59ccf1bf90d26ff5e1051b368942f19f
Ruziyev_Nadirxan <nodirruziyev381@gmail.com>
2023-08-16 21:35:17 +0500

    №3 Amaliyo 6ta masala

--- Masalalar .js
+++ Masalalar .js
@@ -94,3 +94,124 @@
 //     yozgan belgilaringiz soni ${p.length}`);
 // }
 
+// // Amaliyo_3----------------------------------------------------------------
+// //1-misol
+// //1-usul
+// const array = [[false , true] , [ 1 , 2] , 99 , 'yaxshi' ];
+// for(let i= 0; i < array.length ;i++){
+//     if(typeof array[i] == 'string'){
+//         alert(`qiymat : ${array[i]}   index:${i}`)
+//     };
+// }
+
+// // 2-usul
+// array.forEach((item,index)=>{
+//     if(typeof item == 'string'){
+//         console.log(index);
+//     }
+// })
+
+// // 3- usul
+// for(let  key of array){
+//     if(typeof key == 'string'){
+//         console.log(array.indexOf(key));
+//     }
+// }
+
+// // 2-misol
+// // 1-usul
+// const data = [[1,2,3] ,'qizil' , [4,5,6] ,true,false];
+// let numdata = []
+// for(let key of data){
+//     if(Array.isArray(key)){
+//         numdata.push(key)
+//     }
+// }
+// numdata = numdata[0].concat(numdata[1]);
+// console.log(numdata)
+
+// // 2-usul
+// data.forEach((item)=>{
+//     if(Array.isArray(item)){
+//         numdata.push(item)
+//      }
+// })
+// numdata = numdata[0].concat(numdata[1]);
+// console.log(numdata)
+
+
+// // 3-misol
+// // 1- usul
+// const array = [ [1,2,3,4,5] , [6,7,8,9],[9,8,7,6,5,4], [1,2,3,4,5,6,7,8,9]];
+// const allnum = [];
+// for(let i = 0 ; i < array.length;i++){
+//     for(let k = 0; k < array[i].length; k++){
+//         allnum.push(array[i][k]);
+//     }
+// }
+// console.log(allnum);
+
+// //2-usul
+// const array = [ [1,2,3,4,5] , [6,7,8,9],[9,8,7,6,5,4]];
+// const allnum = [];
+// array.forEach((item)=>{
+//      item.forEach((item)=>{
+//         allnum.push(item);
+//      })
+// })
+// console.log(allnum);
+
+// // 4-misol
+// const checkdata = [null, 'false' , 0 , 99 , false , undefined, '',NaN, '0'];
+// const newfalse = [];
+// checkdata.forEach((item)=>{
+//     if(!item){
+//         newfalse.push(item);
+//     };
+// })
+// console.log(newfalse);
+
+// // 5-misol
+// // 1-usul
+// const text = prompt();
+// const massiv = text.split('');
+// const revers =[]
+// massiv.forEach((item)=>{
+//     revers.unshift(item);
+// })
+// console.log(revers.join(''));
+
+// // 2-usul
+// const text = "Nodir";
+// let revers ='';
+// for(let i=0 ; i<text.length;i++){       //0 1 2 3 4
+//     revers+=text[(text.length-1)-i]  // 4 3 2 1 0
+// }
+// console.log(revers)
+
+// // 3 - usul
+// const text = 'Nodir';
+// let revtext = '';
+// for(let i =text.length - 1; i>=0 ; i--){
+//     revtext +=text[i];
+// }
+// console.log(revtext);
+
+// //4-usul
+// const text = 'Nodir';
+// const arrtext = text.split('').reverse();
+// const revtext = arrtext.join('');
+// console.log(revtext);
+
+// // 6-misol
+// let Word = 'AssALOmu aLeyKUm YaxshiMisiZ';
+// let wordMas = Word.split(' ');
+// let javob =[];
+// let correct ='';
+// wordMas.forEach((item)=>{
+//     javob.push(item[0].toLowerCase() + item.slice(1).toUpperCase());
+// })
+// javob.forEach((item)=>{
+//     correct +=item +' '
+// })
+// console.log(correct.trim());    
